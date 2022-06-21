60fe196d8b37ecee076c9a56f7912b18f63dcc65
lecosh <artem_vorobey_2001@mail.ru>
2022-06-21 17:46:15 +0300

    added pesponse for hotel section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
-  
+    simulateTouch: false,
     // Navigation arrows
     navigation: {
       nextEl: '.slider-button--next',
