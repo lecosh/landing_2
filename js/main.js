95c9d1265f3608c35fb025f98b085d22db5d3b2f
lecosh <artem_vorobey_2001@mail.ru>
2022-06-21 15:28:41 +0300

    added slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+    },
+  });
