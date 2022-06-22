cba484a3ac3da1b7107f5a2bffd8e41dff756c00
lecosh <artem_vorobey_2001@mail.ru>
2022-06-22 16:50:09 +0300

    added another slider and section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
-    simulateTouch: false,
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
     },
   });
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+});
