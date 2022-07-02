81ff4ae3cff123ad128ae02a3dac0c8f32d2136e
lecosh <artem_vorobey_2001@mail.ru>
2022-07-02 13:31:12 +0300

    added modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function(){
+  const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
@@ -8,17 +9,37 @@ const hotelSlider = new Swiper('.hotel-slider', {
       prevEl: '.hotel-slider__button--prev',
     },
   });
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
-const menuButton = document.querySelector('.menu-button')
-menuButton.addEventListener('click', ()=>{
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+  const menuButton = document.querySelector('.menu-button')
+  menuButton.addEventListener('click', ()=>{
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+  })
+
+
+  let modalButton = $("[data-toggle=modal]")
+  let modalClose = $(".modal__close")
+  modalButton.on("click", openModal)
+  modalClose.on("click", closeModal)
+  function openModal(){
+    let modalOverlay = $('.modal__overlay');
+    let modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible')
+    modalDialog.addClass('modal__dialog--visible')
+  }
+  function closeModal(event){
+    event.preventDefault()
+    let modalOverlay = $('.modal__overlay');
+    let modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible')
+    modalDialog.removeClass('modal__dialog--visible')
+  }
 })
